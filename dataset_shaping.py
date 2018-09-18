c18e4a299ec5c98690b0ed56a3fbc8ae02a77709
Raul Kist <Raul.Kist@cegal.com>
2018-09-18 12:01:32 +0200

    inside pol added

--- dataset_shaping.py
+++ dataset_shaping.py
@@ -3,21 +3,7 @@
 @author: dhakk
 """
 
-
-import pandas as pd
-
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-
-###PATHS###
-FILE_ROOT='C:\\Users\\hakkad\\Documents\\GitHub\\lostin4Dspace\\'
-
-LOFS_RAW_FILENAME='LOFS4D.xlsx'
-STREAMERS_RAW_FILENAME='Streamer4D.xlsx'
-PRESSURES_FILENAME='PressureTargets.csv'
-
-PRESSURES_TARGET_OUTPUT_FILENAME='Dataset.csv'
+from configurations import *
 
 ######
 
