4d19cc6bf3cc773257cc9c9d98e48a2a2214659b
Raul Kist <Raul.Kist@cegal.com>
2018-09-18 15:32:47 +0200

    main added

--- dataset_shaping.py
+++ dataset_shaping.py
@@ -8,85 +8,87 @@ import random
 from configurations import FILE_ROOT,PRESSURES_FILENAME,LOFS_RAW_FILENAME,STREAMERS_RAW_FILENAME,DATASET_OUTPUT_FILENAME
 from configurations import TRAIN_DATASET_FILENAME,TEST_DATASET_FILENAME,TRAIN_PARTITION_PCT
 
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 
-###PATHS###
-######
+def SplitAndCleanData():
+    """split and clean"""
 
-pressuresDf= pd.read_csv(FILE_ROOT+PRESSURES_FILENAME)
-
-pressuresDf.describe()
-
-lofsDf=pd.read_excel(FILE_ROOT+LOFS_RAW_FILENAME)
-lofsDf.columns=[x.replace('_TVT','_') for x in lofsDf.columns]
-
-streamersDf=pd.read_excel(FILE_ROOT+STREAMERS_RAW_FILENAME)
-streamersDf.columns=[x.replace('_TVT','_') for x in streamersDf.columns]
-
-datasetDf=pressuresDf.copy()
-print("After pressure target, row count:"+str(len(datasetDf.index)))
-
-uniqueCases=datasetDf.DeltaCase.unique()
+    pd.set_option('display.max_rows', 500)
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
 
 
-caseList=[]
-for case in uniqueCases:
-    if case.startswith('S'):
-        #Streamer
-        base=case.split('minus')[1]
-        monitor=case.split('minus')[0]
-        co_columnname='4D CO'+monitor+"-"+base
-        ai_columnname='4D AI'+monitor+"-"+base
-        
-        if co_columnname not in streamersDf.columns:
-            print("Column name missing:"+co_columnname)
-        if ai_columnname not in streamersDf.columns:
-            print("Column name missing:"+co_columnname)
-        
-        tmpDf=streamersDf[['Well','Z',co_columnname,ai_columnname]]
-        tmpDf.columns=['Well','Z','co','ai']
-        tmpDf['DeltaCase']=case
-        caseList.append(tmpDf)
-    elif case.startswith('L'):
-        base=case.split('minus')[1]
-        monitor=case.split('minus')[0]
-        co_columnname='4D CO_'+monitor+"-"+base+"_"
-        ai_columnname='4D AI_'+monitor+"-"+base+"_"
-        if co_columnname not in lofsDf.columns:
-            print("Column name missing:"+co_columnname)
-        if ai_columnname not in lofsDf.columns:
-            print("Column name missing:"+co_columnname)
-        
-        tmpDf=lofsDf[['Well','Z',co_columnname,ai_columnname]]
-        tmpDf.columns=['Well','Z','co','ai']
-        tmpDf['DeltaCase']=case
-        caseList.append(tmpDf)
-    else:
-        raise ValueError('Bad case:'+case)
+    pressuresDf= pd.read_csv(FILE_ROOT+PRESSURES_FILENAME)
+
+    pressuresDf.describe()
+
+    lofsDf=pd.read_excel(FILE_ROOT+LOFS_RAW_FILENAME)
+    lofsDf.columns=[x.replace('_TVT','_') for x in lofsDf.columns]
+
+    streamersDf=pd.read_excel(FILE_ROOT+STREAMERS_RAW_FILENAME)
+    streamersDf.columns=[x.replace('_TVT','_') for x in streamersDf.columns]
+
+    datasetDf=pressuresDf.copy()
+    print("After pressure target, row count:"+str(len(datasetDf.index)))
+
+    uniqueCases=datasetDf.DeltaCase.unique()
 
 
-caseDf=pd.concat(caseList)
-datasetDf=pd.merge(datasetDf,caseDf,on=['Well','Z','DeltaCase'])
-print("After ai and co features, row count:"+str(len(datasetDf.index)))
-datasetDf=pd.merge(datasetDf,streamersDf[['Well','Z','4D qual Fact']], on =['Well','Z'])
-print("After quality features, row count:"+str(len(datasetDf.index)))
+    caseList=[]
+    for case in uniqueCases:
+        if case.startswith('S'):
+            #Streamer
+            base=case.split('minus')[1]
+            monitor=case.split('minus')[0]
+            co_columnname='4D CO'+monitor+"-"+base
+            ai_columnname='4D AI'+monitor+"-"+base
+            
+            if co_columnname not in streamersDf.columns:
+                print("Column name missing:"+co_columnname)
+            if ai_columnname not in streamersDf.columns:
+                print("Column name missing:"+co_columnname)
+            
+            tmpDf=streamersDf[['Well','Z',co_columnname,ai_columnname]]
+            tmpDf.columns=['Well','Z','co','ai']
+            tmpDf['DeltaCase']=case
+            caseList.append(tmpDf)
+        elif case.startswith('L'):
+            base=case.split('minus')[1]
+            monitor=case.split('minus')[0]
+            co_columnname='4D CO_'+monitor+"-"+base+"_"
+            ai_columnname='4D AI_'+monitor+"-"+base+"_"
+            if co_columnname not in lofsDf.columns:
+                print("Column name missing:"+co_columnname)
+            if ai_columnname not in lofsDf.columns:
+                print("Column name missing:"+co_columnname)
+            
+            tmpDf=lofsDf[['Well','Z',co_columnname,ai_columnname]]
+            tmpDf.columns=['Well','Z','co','ai']
+            tmpDf['DeltaCase']=case
+            caseList.append(tmpDf)
+        else:
+            raise ValueError('Bad case:'+case)
 
-print("Unique wells: "+ str(len(datasetDf.Well.unique())))
 
-datasetDf.to_csv(FILE_ROOT+DATASET_OUTPUT_FILENAME)
+    caseDf=pd.concat(caseList)
+    datasetDf=pd.merge(datasetDf,caseDf,on=['Well','Z','DeltaCase'])
+    print("After ai and co features, row count:"+str(len(datasetDf.index)))
+    datasetDf=pd.merge(datasetDf,streamersDf[['Well','Z','4D qual Fact']], on =['Well','Z'])
+    print("After quality features, row count:"+str(len(datasetDf.index)))
 
-uniqueWells=datasetDf.Well.unique()
+    print("Unique wells: "+ str(len(datasetDf.Well.unique())))
 
-random.shuffle(uniqueWells)
-trainWells=uniqueWells[range(int(TRAIN_PARTITION_PCT*len(uniqueWells)))]
+    datasetDf.to_csv(FILE_ROOT+DATASET_OUTPUT_FILENAME)
 
-trainDf=datasetDf[[x in trainWells for x in datasetDf.Well]]
-testDf=datasetDf[[x not in trainWells for x in datasetDf.Well]]
+    uniqueWells=datasetDf.Well.unique()
 
-trainDf.to_csv(FILE_ROOT+TRAIN_DATASET_FILENAME)
-testDf.to_csv(FILE_ROOT+TEST_DATASET_FILENAME)
+    random.shuffle(uniqueWells)
+    trainWells=uniqueWells[range(int(TRAIN_PARTITION_PCT*len(uniqueWells)))]
+
+    trainDf=datasetDf[[x in trainWells for x in datasetDf.Well]]
+    testDf=datasetDf[[x not in trainWells for x in datasetDf.Well]]
+
+    trainDf.to_csv(FILE_ROOT+TRAIN_DATASET_FILENAME)
+    testDf.to_csv(FILE_ROOT+TEST_DATASET_FILENAME)
 
 
 
--- test_case01.py
+++ test_case01.py
@@ -1,50 +1,54 @@
 from configurations import *
 
 
-trainDf= pd.read_csv(FILE_ROOT+TRAIN_DATASET_FILENAME)
-testDf= pd.read_csv(FILE_ROOT+TEST_DATASET_FILENAME)
+def RandomForestTheData():
+    """run the first stuff"""
+    trainDf= pd.read_csv(FILE_ROOT+TRAIN_DATASET_FILENAME)
+    testDf= pd.read_csv(FILE_ROOT+TEST_DATASET_FILENAME)
 
-print(trainDf.head())
-print(trainDf.index)
+    print(trainDf.head())
+    print(trainDf.index)
 
-print(testDf.head())
-print(testDf.index)
-
-
-#define 
-regr = RandomForestRegressor(max_depth=4, random_state=0)
-
-trainingColumns = [ "x", "y", "MD","Z",  "PTI_TVT", "co", "ai"]
-
-Xtrain = trainDf[trainingColumns]
-Ytrain = trainDf["DeltaPressure"]
-Wtrain = trainDf["4D qual Fact"]
-
-Xtest = testDf[trainingColumns]
-Ytest = testDf["DeltaPressure"]
-Wtest = testDf["4D qual Fact"]
-
-print(Xtrain.head())
-print(Ytrain.head())
-
-regr.fit(Xtrain, Ytrain, sample_weight=Wtrain)
-
-# RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=2,
-#            max_features='auto', max_leaf_nodes=None,
-#            min_impurity_decrease=0.0, min_impurity_split=None,
-#            min_samples_leaf=1, min_samples_split=2,
-#            min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1,
-#            oob_score=False, random_state=0, verbose=0, warm_start=False)
-
-Ypredicted = regr.predict(Xtest)
-
-print(len(Ypredicted))
-print(len(Ytest))
+    print(testDf.head())
+    print(testDf.index)
 
 
-#score
-print(regr.score(Xtest, Ytest, sample_weight=Wtest))
+    #define 
+    regr = RandomForestRegressor(max_depth=4, random_state=0)
 
-print(score_medium(Ytest, Ypredicted))
-plot_scatter(Ytest, Ypredicted, Wtest)
+    trainingColumns = [ "x", "y", "MD","Z",  "PTI_TVT", "co", "ai"]
+
+    Xtrain = trainDf[trainingColumns]
+    Ytrain = trainDf["DeltaPressure"]
+    Wtrain = trainDf["4D qual Fact"]
+
+    Xtest = testDf[trainingColumns]
+    Ytest = testDf["DeltaPressure"]
+    Wtest = testDf["4D qual Fact"]
+
+    print(Xtrain.head())
+    print(Ytrain.head())
+
+    regr.fit(Xtrain, Ytrain, sample_weight=Wtrain)
+
+    # RandomForestRegressor(bootstrap=True, criterion='mse', max_depth=2,
+    #            max_features='auto', max_leaf_nodes=None,
+    #            min_impurity_decrease=0.0, min_impurity_split=None,
+    #            min_samples_leaf=1, min_samples_split=2,
+    #            min_weight_fraction_leaf=0.0, n_estimators=10, n_jobs=1,
+    #            oob_score=False, random_state=0, verbose=0, warm_start=False)
+
+    Ypredicted = regr.predict(Xtest)
+
+    print(len(Ypredicted))
+    print(len(Ytest))
+
+
+    #score
+    print(regr.score(Xtest, Ytest, sample_weight=Wtest))
+
+    print(score_medium(Ytest, Ypredicted))
+    plot_scatter(Ytest, Ypredicted, Wtest)
+
+    
 
